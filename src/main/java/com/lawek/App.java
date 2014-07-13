f453a95ec0cf21e83c6344e0f23a71023d107a19
mlkdru <malik@lawek.com>
2014-07-13 23:36:41 +0200

    dfgdfg

--- src/main/java/com/lawek/App.java
+++ src/main/java/com/lawek/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
+    	System.out.println( "fsdf" );
+    	
+    	
         System.out.println( "Hello World! Dru" );
     }
 }
