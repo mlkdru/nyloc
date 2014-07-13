b189cd25ad451d2c4a73177cfe24a9eeaae31117
mlkdru <malik@lawek.com>
2014-07-13 23:15:56 +0200

    pom.xml maven de base

---
+++ src/main/java/com/lawek/App.java
@@ -0,0 +1,13 @@
+package com.lawek;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
